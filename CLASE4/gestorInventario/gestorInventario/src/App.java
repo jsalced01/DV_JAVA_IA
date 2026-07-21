e82b560c9b9effd3cd67bd8cf43c5633bdc87713
jsalced0 <jsfox0001@hotmail.com>
2026-07-21 17:55:33 -0500

    Modificacion de App.java

--- CLASE4/gestorInventario/gestorInventario/src/App.java
+++ CLASE4/gestorInventario/gestorInventario/src/App.java
@@ -30,6 +30,6 @@ public class App {
                     System.out.println("Opcion no valida. Elige entre 1 y 5.");
             }
         }
-        sc.close();//Cerrar el Scanner
+        sc.close();//Cerrar el Scanner 05:44
     }
 }
